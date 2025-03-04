093c8bd43c2ea2ebfb88af3db0794a3fca5d4138
Mousam bathre <bathremousam@gmail.com>
2025-03-04 16:02:02 +0530

    [Mousam Bathre] adding the UC_3 adding DTO and model class

---
+++ AddressbookAppDevelopment/src/main/java/com/spring/AddressbookAppDevelopment/DTO/ContactDTO.java
@@ -0,0 +1,10 @@
+package com.spring.AddressbookAppDevelopment.DTO;
+
+import lombok.Data;
+
+public @Data class ContactDTO {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+}
--- AddressbookAppDevelopment/src/main/java/com/spring/AddressbookAppDevelopment/model/Contact.java
+++ AddressbookAppDevelopment/src/main/java/com/spring/AddressbookAppDevelopment/model/Contact.java
@@ -1,13 +1,28 @@
 package com.spring.AddressbookAppDevelopment.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.annotation.processing.Generated;
+
 
 @Slf4j
-
+@Entity
+@Table(name = "contacts")
+@NoArgsConstructor
 public @Data class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true ,nullable = false)
     private String email;
+    @Column(unique = true, nullable = false)
     private String phone;
 }
